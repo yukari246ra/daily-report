247e159fbe21b41f5d0311f8f19bd9d93ee644e1
yukari246ra <yukari246ra@gmail.com>
2026-03-13 21:21:06 +0900

    7-10

---
+++ vite-project/src/main.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
+import { fetchHistoryData } from "./my-modules/fetch-history-data";
+import { submitData } from "./my-modules/submit-data";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Cloud Firestoreの初期化
+const db = getFirestore(app);
+
+//Cloud Firestoreから取得したデータを表示する
+if (document.getElementById("js-history")) {
+    fetchHistoryData(getDocs, collection, db);
+}
+
+// Cloud Firestoreにデータを送信する
+if(document.getElementById("js-form")) {
+    document.getElementById("js-form").addEventListener("submit", (e) => submitData(e, addDoc, collection, db));
+}
---
+++ vite-project/src/my-modules/fetch-history-data.js
@@ -0,0 +1,13 @@
+export const fetchHistoryData = async (getDocs, collection, db) => {
+    let tags ="";
+
+    //reportsコレクションのデータを取得
+    const querySnapshot = await getDocs(collection(db, "reports"));
+
+    //データをテーブル表の形式に合わせてHTMLに挿入
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        tags += `<tr><td>${doc.data().date}</td><td>${doc.data().name}</td><td>${doc.data().work}</td><td>${doc.data().comment}</td></tr>`
+    });
+    document.getElementById("js-history").innerHTML = tags;
+};
